2c5b192be421e24c0de3c57381458b16fa6dc78e
AISimonetta <info@simonettaalejandra.com>
2023-12-04 16:37:56 +0000

    working on the scss atributes for the cards__flips done

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,18 @@
 import './main.scss';
 
+const cards = document.querySelectorAll<HTMLDivElement>(".cardContent");
+
+if (!cards) {
+    throw new Error("There is aproblem with the selector");
+    
+}
+
+//flip cards
+const flipCards = (card : HTMLDivElement ) => {
+    card.classList.toggle("cards__flip");
+}
+cards.forEach(card => card.addEventListener("click", () => {
+    flipCards(card)
+}));
+
+//matching cards :
