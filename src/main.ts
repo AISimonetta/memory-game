8fbd47e0f4a92ebff66ae105c244770f961d7c63
AISimonetta <info@simonettaalejandra.com>
2023-12-06 23:53:42 +0000

    match funcion added, resetFlippedCards added and working

--- src/main.ts
+++ src/main.ts
@@ -30,33 +30,81 @@ if (!cards) {
     throw new Error("There is a problem with the selector");
 }
 
+const maxFlippedCards = 2;
 let flippedCards : number = 0;
-// let firstFlippedCard =
-// let secondFlippedCard:
+let firstFlippedCard : string | null;
+let secondFlippedCard : string | null;
 
 const flipCard = (card: HTMLDivElement) => {
-    console.log(' CARD ',  card.children);
+    // console.log(' CARD ',  card.children[1].getAttribute("src"));
+
+    // if (flippedCards === maxFlippedCards) {
+    //     return
+    // }  else {
+    //     card.classList.toggle("cards__flipped");
+    //     if (flippedCards === 0) {
+    //         firstFlippedCard = card.children[1].getAttribute("src");
+    //     }
+    
+    //     if (flippedCards === 1) {
+    //         secondFlippedCard = card.children[1].getAttribute("src");
+    //     }
+    // }
+
+
+    if (flippedCards < maxFlippedCards) {
+        card.classList.toggle("cards__flipped");
+        if (flippedCards === 0) {
+            firstFlippedCard = card.children[1].getAttribute("src");
+        }
+    
+        if (flippedCards === 1) {
+            secondFlippedCard = card.children[1].getAttribute("src");
+        }
+    } else {
+        return;
+    }
+
+    flippedCards = flippedCards + 1;
 
     if (flippedCards === 2) {
-        return
-    } else {
-        card.classList.toggle("cards__flipped");
+        checkCardsMatch(firstFlippedCard, secondFlippedCard);
     }
-    flippedCards = flippedCards + 1;
 }
 
-// const checkCardsMatch = (card: HTMLDivElement) => {
-//     var youtubeimgsrc = document.getElementById("youtubeimg").src;
-// }
+const checkCardsMatch = (firstFlippedCard: string | null, secondFlippedCard: string | null) => {
+    console.log('TEST ', firstFlippedCard);
+    console.log('TEST ', secondFlippedCard);
 
+    const matchedCards = document.querySelectorAll<HTMLDivElement>(".cards__flipped");
 
+    if (firstFlippedCard === secondFlippedCard) {
+        // alert("match !!")
 
-// cards.forEach(card => card.
+        matchedCards.forEach(card =>
+            card.classList.toggle("cards__matched")
+        );
+        resetFlippedCards();
+    } else {
+        console.log('TKFKFDSNFKDNT ');
 
-// );
+        setTimeout(() => {
+            matchedCards.forEach(card =>
+                card.classList.remove("cards__flipped")
+            )
+        }, 1500);
 
-cards.forEach(card=> card.addEventListener("click", () => {
+        resetFlippedCards();
+    }
+};
 
+const resetFlippedCards = () => {
+    flippedCards = 0;
+    firstFlippedCard = "";
+    secondFlippedCard = "";
+}
+
+cards.forEach(card => card.addEventListener("click", () => {
     flipCard(card);
 }));
 
