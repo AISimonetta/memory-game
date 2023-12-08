3086e8add956ec37b8367a3af91807e68b986eba
AISimonetta <info@simonettaalejandra.com>
2023-12-08 08:49:23 +0000

    function to order cards randmply while pressing play again done and  working

--- src/main.ts
+++ src/main.ts
@@ -23,10 +23,12 @@ const friendCards = (friendsArray: Friends[]): string => {
 
 cardContainerAll.innerHTML = friendCards(friendsArray)
 
-/////////// cards
+//******** MEMORY CARDS ******/
 const cards = document.querySelectorAll<HTMLDivElement>(".cardContent");
+const buttonReset = document.querySelector<HTMLButtonElement>(".btn");
 
-if (!cards) {
+
+if (!cards || !buttonReset) {
     throw new Error("There is a problem with the selector");
 }
 
@@ -36,73 +38,71 @@ let firstFlippedCard : string | null;
 let secondFlippedCard : string | null;
 
 const flipCard = (card: HTMLDivElement) => {
-    // console.log(' CARD ',  card.children[1].getAttribute("src"));
-
-    // if (flippedCards === maxFlippedCards) {
-    //     return
-    // }  else {
-    //     card.classList.toggle("cards__flipped");
-    //     if (flippedCards === 0) {
-    //         firstFlippedCard = card.children[1].getAttribute("src");
-    //     }
-    
-    //     if (flippedCards === 1) {
-    //         secondFlippedCard = card.children[1].getAttribute("src");
-    //     }
-    // }
-
-
     if (flippedCards < maxFlippedCards) {
         card.classList.toggle("cards__flipped");
         if (flippedCards === 0) {
             firstFlippedCard = card.children[1].getAttribute("src");
         }
-    
         if (flippedCards === 1) {
             secondFlippedCard = card.children[1].getAttribute("src");
         }
     } else {
         return;
     }
-
     flippedCards = flippedCards + 1;
-
     if (flippedCards === 2) {
         checkCardsMatch(firstFlippedCard, secondFlippedCard);
     }
 }
-
 const checkCardsMatch = (firstFlippedCard: string | null, secondFlippedCard: string | null) => {
-    console.log('TEST ', firstFlippedCard);
-    console.log('TEST ', secondFlippedCard);
-
+    console.log("TEST", firstFlippedCard);
+    console.log("TEST", secondFlippedCard);
     const matchedCards = document.querySelectorAll<HTMLDivElement>(".cards__flipped");
-
     if (firstFlippedCard === secondFlippedCard) {
-        // alert("match !!")
-
         matchedCards.forEach(card =>
             card.classList.toggle("cards__matched")
         );
+        //asd some conffetti!!
         resetFlippedCards();
+        // lockCards();
     } else {
-        console.log('TKFKFDSNFKDNT ');
-
+        //toggle screen red or black
+        console.log('NOMATCH ');
         setTimeout(() => {
-            matchedCards.forEach(card =>
+            matchedCards.forEach(card => {
+               
                 card.classList.remove("cards__flipped")
-            )
-        }, 1500);
+            }
 
-        resetFlippedCards();
-    }
-};
-
+            );
+            resetFlippedCards();
+        }, 1000);
+    };
+}
 const resetFlippedCards = () => {
     flippedCards = 0;
-    firstFlippedCard = "";
-    secondFlippedCard = "";
+    firstFlippedCard = null;
+    secondFlippedCard = null;
 }
+
+// const lockCards =() => {
+//     cards.forEach(card  => {
+//         card.removeEventListener("click",flipCard)
+// })
+// }
+
+// const resetGame = () => {
+//     flippedCards = 0;
+//     firstFlippedCard = null;
+//     secondFlippedCard = null;
+
+//     const cards = document.querySelectorAll<HTMLDivElement>(".cardContent");
+//         cards.forEach(card  => {
+//             let randomOrder = Math.floor(Math.random() * 12);
+//             card.style.order = randomOrder.toString();
+//     })
+// }
+// buttonReset.addEventListener("click", resetGame)
 
 cards.forEach(card => card.addEventListener("click", () => {
     flipCard(card);
@@ -110,51 +110,3 @@ cards.forEach(card => card.addEventListener("click", () => {
 
 
 
-// let firstFlippedCard: HTMLDivElement | null = null;
-// let secondFlippedCard: HTMLDivElement | null = null;
-// let hasFlippedCard = false;
-// let lockFlipping = false;
-
-
-// const checkFlip = (card: HTMLDivElement) => {
-//     if (lockFlipping) return;
-//     if (firstFlippedCard === card) return; // not to click twice
-//         card.classList.toggle("cards__flip");
-//     if (!hasFlippedCard) {
-//         hasFlippedCard = true;
-//         firstFlippedCard = card;
-//         return;
-//     }
-//     secondFlippedCard = card;
-// }
-
-// cards.forEach(card=> card.addEventListener("click", () => {
-//     checkFlip(card);
-// }));
-
-// //compare an ids !
-// // const checkIfMatch = () => {
-// //     let match = firstFlippedCard. === secondFlippedCard;
-// //     if (match) {
-// //         disableCards()
-// //     } else {
-// //         cardsUnflip()
-// //     }
-// // }
-
-// // const disableCards = () => {
-// //     firstCard.removeEventListener("click", checkFlip);
-// //     secondCard.removeEventListener("click", checkFlip);
-// // }
-
-// // const resetCards = () => {
-
-// // }
-
-// // const cardsUnflip = () => {
-// // }
-
-// // const randomOrder = () => {
-//     // Math.floor(Math.random() * 12)
-
-// // }
